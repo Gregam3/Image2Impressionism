fcdf2a81adf0c70e845a19a524925bc2592a53ca
Gregam3 <gregoryamitten@gmail.com>
2020-07-02 21:42:14 +0200

    Progress on pixel moving

--- src/main/java/com/greg/PixelMoveType.java
+++ src/main/java/com/greg/PixelMoveType.java
@@ -4,8 +4,11 @@ import java.awt.image.BufferedImage;
 
 public enum PixelMoveType {
     RIGHT(pixel -> new Pixel(pixel.getX() + 1, pixel.getY())),
+    RIGHT_DOWN(pixel -> new Pixel(pixel.getX() + 1, pixel.getY() - 1)),
     DOWN(pixel -> new Pixel(pixel.getX(), pixel.getY() - 1)),
-    LEFT(pixel -> new Pixel(pixel.getX() - 1, pixel.getY() + 1)),
+    DOWN_LEFT(pixel -> new Pixel(pixel.getX() - 1, pixel.getY() - 1)),
+    LEFT(pixel -> new Pixel(pixel.getX() - 1, pixel.getY())),
+    LEFT_UP(pixel -> new Pixel(pixel.getX() -1, pixel.getY() + 1)),
     UP(pixel -> new Pixel(pixel.getX(), pixel.getY() + 1));
 
     private final PixelMove move;
